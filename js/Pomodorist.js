128007aed9a53a2afab12f53a7aadd0807fd7f43
ivananutrition <ivana.arizanovic@hotmail.com>
2017-12-02 11:11:24 +0100

    added part of js and css

--- js/Pomodorist.js
+++ js/Pomodorist.js
@@ -1,149 +1,36 @@
-var minutes = document.getElementById("minutes");
-var seconds = document.getElementById("seconds");
-//var message = document.getElementById("message");
+window.onload = () => {
+    // pocetne vrednosti tajmera
+    var minute = 25;
+    var seconds = 0;
+    var totalSeconds = 1500;
 
-var start = document.getElementById("startbtn");
-start.addEventListener("click", startTimer, false);
+    var intervalId = null;
 
-var stop = document.getElementById("stopbtn");
-stop.addEventListener("click", stopTimer, false);
+    function startTimer() {
+        --totalSeconds;
 
-var reset = document.getElementById("resetbtn");
-reset.addEventListener("click", resetTimer, false);
+        minute = Math.floor((totalSeconds - 1) / 60);
+        seconds = totalSeconds - (minute * 60);
 
 
-var bigTime = 1499; // time in seconds
-var mode = "normal";
-var animation = "fadeToBlack";
-
-var color = "0D5B85";
-var percent;
-
-var mins;
-var secs;
-
-var countdownID;
-
-// get all the elements
-var minutes = document.getElementById("minutes");
-var seconds = document.getElementById("seconds");
-var message = document.getElementById("message");
-
-var test = document.getElementById("test");
-
-// register the buttons
-var start = document.getElementById("start");
-start.addEventListener("click", startTimer, false);
-
-var stop = document.getElementById("stop");
-stop.addEventListener("click", stopTimer, false);
-
-var reset = document.getElementById("reset");
-reset.addEventListener("click", resetTimer, false);
-
-// COUNTER ========================================================
-function counter() {
-
-    // calculate the minutes and seconds from bigTime
-    mins = Math.floor(bigTime / 60);
-    secs = bigTime - mins * 60;
-
-    // change the HTML to show new minutes and seconds
-    minutes.innerHTML = (mins < 10 ? '0' : '') + mins;
-    seconds.innerHTML = (secs < 10 ? '0' : '') + secs;
-
-    // handle the animations
-    var divisor = 300;
-
-    percent = secs / divisor;
-    color = shadeColor(color, -percent);
-    document.body.style.background = "#" + color;
-    test.innerHTML = color;
-    divisor - 100;
-
-    // change the message at 00
-    if (secs == 0) {
-        message.innerHTML = "change out the messages";
+        document.getElementById("minute").innerHTML = minute;
+        document.getElementById("seconds").innerHTML = seconds;
     }
 
-    // switch modes if timer ends
-    if (bigTime == 0) {
+    document.getElementById('start-btn').addEventListener('click', () => {
+        intervalId = setInterval(startTimer, 1500);
+    })
 
-        if (mode == "normal") {
-
-            // cooldown is 5min 
-            mode = "cooldown";
-            bigTime = 300;
-
-        } else if (mode == "cooldown") {
-
-            // switch back to normal 25min mode
-            mode = "normal";
-            bigTime = 1499;
-
-            minutes.innerHTML = "25";
-            seconds.innerHTML = "00";
-
-            document.body.style.background = "#" + color;
-
-            // show start button
-            start.style.display = "block";
-            stop.style.display = "none";
-            reset.style.display = "none";
-
-            // stop timer
-            clearInterval(countdownID);
-        }
-
-    } else {
-        // decrement
-        bigTime = bigTime - 1;
-    }
-
-}
+    document.getElementById('stop-btn').addEventListener('click', () => {
+        if (intervalId)
+            clearInterval(intervalId);
+    });
 
 
+    document.getElementById('reset-btn').addEventListener('click', () => {
+        totalSeconds = 1500;
 
-
-
-function start() {
-
-    countdownID = setInterval("counter()", 10);
-
-
-    start.style.display = "none";
-    stop.style.display = "block";
-    reset.style.display = "none";
-}
-
-
-function end() {
-
-
-    clearInterval(countdownID);
-
-
-    start.style.display = "none";
-    stop.style.display = "none";
-    reset.style.display = "block";
-}
-
-
-function reset() {
-
-    bigTime = 1499;
-
-
-    start.style.display = "block";
-    stop.style.display = "none";
-    reset.style.display = "none";
-}
-
-function shadeColor(color, percent) {
-    var num = parseInt(color, 16),
-        amt = Math.round(2.55 * percent),
-        R = (num >> 16) + amt,
-        B = (num >> 8 & 0x00FF) + amt,
-        G = (num & 0x0000FF) + amt;
-    return (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1);
+        document.getElementById("minute").innerHTML = '25';
+        document.getElementById("seconds").innerHTML = '00';
+    });
 }
